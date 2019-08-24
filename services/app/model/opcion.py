c49f96104580e57da1adedc1da44747c90fc0149
Franco <csirfranco@gmail.com>
2019-08-23 23:29:04 -0300

    reset migraciones

---
+++ services/app/model/opcion.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.sql.schema import ForeignKey
+import enum
+from app.extensions import db
+
+class Opcion(db.Model):
+    __tablenombreOpcion__ = 'opcion'
+    codOpcion = db.Column('cod_opcion',Integer, primary_key = True, index=True)
+    nombreOpcion = db.Column('nombre_opcion',String(60), nullable=False,unique=True)
+    isActiv = db.Column('is_activ',Boolean,nullable=True)
+
+
+    def __init__(self, nombreOpcion, isActiv):
+        self.nombreOpcion = nombreOpcion
+        self.isActiv = isActiv
+    
+
+    @staticmethod
+    def from_json(json):
+        opcion = Opcion(
+            nombreOpcion=json.get('nombreOpcion'),
+            isActiv=json.get('isActiv')
+            )
+        return opcion
+
+    def to_json(self):
+        return {
+            'codOpcion': self.codOpcion,
+            'nombreOpcion': self.nombreOpcion,
+            'isActiv': self.isActiv
+        }
