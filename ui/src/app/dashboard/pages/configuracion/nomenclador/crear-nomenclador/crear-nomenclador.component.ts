fe0784a3c7d336dd9705991f55b97d0a321daca5
Jafi <jafibravin@outlook.com>
2019-09-07 17:02:35 -0300

    WIP component crear-nomenclador

--- ui/src/app/dashboard/pages/configuracion/nomenclador/crear-nomenclador/crear-nomenclador.component.ts
+++ ui/src/app/dashboard/pages/configuracion/nomenclador/crear-nomenclador/crear-nomenclador.component.ts
@@ -6,9 +6,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './crear-nomenclador.component.html'
 })
 export class CrearNomencladorComponent implements OnInit {
-  nameInput: string;
-  activoInput: boolean;
-  tipoNomencladorInput: string;
+  nombre: string;
+  tipoNomenclador: boolean;
+  isActiv: string;
   obj: any[];
 
   constructor(private http: HttpClient) {}
@@ -18,9 +18,9 @@ export class CrearNomencladorComponent implements OnInit {
 
   imprimir( ) {
     let myobj = {
-      nameInput: this.nameInput,
-      tipoNomencladorInput: this.tipoNomencladorInput,
-      activoInput: this.activoInput
+      nombre: this.nombre,
+      tipoNomenclador: this.tipoNomenclador,
+      isActiv: this.isActiv
     };
 
     console.log( JSON.stringify( myobj ) );
@@ -35,8 +35,9 @@ export class CrearNomencladorComponent implements OnInit {
     let options = {
       headers: httpHeaders
     };
-
-    let url = 'http://localhost:7000/api/configuracion/nomenclador';
+    
+    // http://localhost:7000/api/configuracion/nomenclador
+    let url = 'http://localhost:9001/api/configuracion/nomenclador';
 
     let myobj = {
       nameInput: this.nameInput,
@@ -44,19 +45,75 @@ export class CrearNomencladorComponent implements OnInit {
       activoInput: this.activoInput
     };
 
+    let objDummy = {
+      "tipoNomenclador": "opcion",
+      "nombre": "123123qas33323213asdased3d",
+      "isActiv": true
+    };
+
     this.http.post(
         url,
-        myobj,
+        obj2,
         options
         ).subscribe( (res: any)  => {
           console.log(res);
           console.log(res.data.nameInput);
-          $('#alerta').popover('show');
         },
         err => {
           console.log(err.status);
           }
         );
 
+  }
+
+  getNomenclador (  ) {
+    let httpHeaders = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+    let options = {
+      headers: httpHeaders
+    };
+    let url = 'http://localhost:9001/api/configuracion/nomenclador/1';
+
+    this.http.get(
+      url,
+      options
+      ).subscribe( (res: any)  => {
+        console.log(res);
+        console.log("nombre" + res.data.nombre);
+        console.log("tipoNomenclador" + res.data.tipoNomenclador);
+        console.log("isActiv" + res.data.isActiv);
+      },
+      err => {
+        console.log("get -> "+ url)
+        console.error("error code: " + err.status + "  " + err.error.message);
+        }
+      );
+  }
+  
+  postNomenclador (  ) {
+    let httpHeaders = new HttpHeaders()
+    .set('Content-Type', 'application/json');
+    let options = {
+      headers: httpHeaders
+    };
+    let url = 'http://localhost:9001/api/configuracion/nomenclador';
+    let obj2 = {
+      "tipoNomenclador": "opcion",
+      "nombre": "dummyObjectOpcion" + Math.floor(Math.random()*10),
+      "isActiv": true
+    };
+
+    this.http.post(
+      url,
+      obj2,
+      options
+      ).subscribe( (res: any)  => {
+        console.log(res);
+      },
+      err => {
+        console.log("post -> "+ url)
+        console.error("error code: " + err.status + "  " + err.error.message);
+        }
+      );
   }
 }
