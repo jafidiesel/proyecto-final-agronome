d21012c26b2cbc0ad729bbd9ab77e3785507780f
Franco <csirfranco@gmail.com>
2019-09-11 22:23:43 -0300

    Se parsean los mensajes y se manejan las exception

--- services/app/api/nomenclador_api.py
+++ services/app/api/nomenclador_api.py
@@ -2,15 +2,10 @@ from flask import jsonify
 from flask_restplus import Resource
 from app.api.helperApi.hlUrl import urlNomenclador
 from app.uses_cases.moduloConfiguracion.gestionarNomenclador import getNomenclador, getNomencladoCod, postNomenclador, putNomenclador
+from app.api.helperApi.hlResponse import ResponseException
 
 
 nomenclador = urlNomenclador
-
-@nomenclador.route('/<string:tipoNomenclador>')
-class NomencladorsHandler(Resource):
-    def get(self,tipoNomenclador):
-        return getNomenclador(tipoNomenclador)
-
 
 @nomenclador.route('')
 class NomencladorsHandler(Resource):
@@ -18,11 +13,21 @@ class NomencladorsHandler(Resource):
         data = self.api.payload
         return postNomenclador(data)
 
+
+@nomenclador.route('/<string:tipoNomenclador>')
+class NomencladorsHandler(Resource):
+    def get(self,tipoNomenclador):
+        return getNomenclador(tipoNomenclador)
+
 @nomenclador.route('/<string:tipoNomenclador>/<int:id>')
 class  NomencladorHandler(Resource):
     def get(self,tipoNomenclador,id):
-        obj = getNomencladoCod(tipoNomenclador,id)
-        return jsonify(obj.to_json())
+        ##tengo que agregar una exceptión ya que el .to_json es parte del objeto, y no puedo econtrar la exceptión primaraia si no encuentro la que to_json no es una funcion de un objeto vacio
+        try:    
+            obj = getNomencladoCod(tipoNomenclador,id)
+            return (obj.to_json())
+        except Exception as e:
+            return ResponseException(e)
 
     def put(self,tipoNomenclador,id):
         data = self.api.payload
