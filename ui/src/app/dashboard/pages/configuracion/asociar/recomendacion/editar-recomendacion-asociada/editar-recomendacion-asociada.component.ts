17d52613836974857b6dc7d47dbf0b52d12299fa
Chechi <chechi.fx2@gmail.com>
2019-09-23 23:19:23 -0300

    Fix merged

---
+++ ui/src/app/dashboard/pages/configuracion/asociar/recomendacion/editar-recomendacion-asociada/editar-recomendacion-asociada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-recomendacion-asociada',
+  templateUrl: './editar-recomendacion-asociada.component.html',
+  styleUrls: ['./editar-recomendacion-asociada.component.css']
+})
+export class EditarRecomendacionAsociadaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
