4be37880e3c35065ca2fce6b14da79553258f686
Jafi <jafibravin@outlook.com>
2019-10-03 08:51:16 -0300

    Añadidas llamadas http al angular service de actividad

--- ui/src/app/dashboard/services/actividad/actividad.service.ts
+++ ui/src/app/dashboard/services/actividad/actividad.service.ts
@@ -26,12 +26,12 @@ export class ActividadService {
   }
 
   /**
- * @param tipoNomenclador string
- * @param isActiv boolean
- * @return Observable<Object>
- *  
- * Devuelve todos los nomencladores activos creados del tipo enviado por parámetro
- */
+   * @param tipoNomenclador string
+   * @param isActiv boolean
+   * @return Observable<Object>
+   *  
+   * Devuelve todos los nomencladores activos creados del tipo enviado por parámetro
+   */
   getListaNomencladoresConFiltro(tipoNomenclador: string, isActiv: boolean): Observable<String> {
     let filtroJson = {
       "filtros":
@@ -41,5 +41,44 @@ export class ActividadService {
     }
 
     return this.http.post<String>(`http://localhost:9001/api/configuracion/nomenclador/${tipoNomenclador}`, filtroJson);
+  }
+
+  /**
+   * @param json any
+   * @return Observable<Object>
+   *  
+   * Realiza una peticion post para registrar una actividad
+   */
+  postActividad(json: any) {
+    return this.http.post(`http://localhost:9001/api/actividad/registrar`, json);
+  }
+
+  /**
+  * @param id number
+  * @return Observable<Object>
+  *  
+  * Realiza un delete para eliminar una actividad registrada (baja lógica)
+  */
+  deleteActividad(id: number) {
+    return this.http.delete(`http://localhost:9001/api/actividad/registrar/${id}`);
+  }
+
+  /**
+  * @return Observable<string>
+  *  
+  * GET obtiene la lista de actividades registradas
+  */
+  getListaActividades(): Observable<any> {
+    return this.http.get<string>(`http://localhost:9001/api/actividad/registrar`);
+  }
+
+  /**
+  * @param id number 
+  * @return Observable<string>
+  *  
+  * GET obtiene la información detallada de una actividad segun el id enviado
+  */
+  getActividad(id: number): Observable<any> {
+    return this.http.get<string>(`http://localhost:9001/api/actividad/registrar/${id}`);
   }
 }
