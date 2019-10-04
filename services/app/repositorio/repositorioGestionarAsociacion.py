4f881655ac6bc7cbdad5413e4da0cda3412636a3
Franco <csirfranco@gmail.com>
2019-10-04 02:00:42 -0300

    ultima refactory asociacion + getparametro full
    queda cerrado el modulo de actividad queda testear

--- services/app/repositorio/repositorioGestionarAsociacion.py
+++ services/app/repositorio/repositorioGestionarAsociacion.py
@@ -1,83 +1,25 @@
 from app.extensions import db
-from app.repositorio.hlDb import Commit, addObject, saveEntidadSinCommit
-#from app.model.hlmodel import 
-from app.model import hlmodel
+from app.model.hlmodel import ActividadParametro, RecomendacionParametro, TipoAnalisisParam, TipoPlanParam, Parametro
 
 
-#def asociaciones()
 
-
-def updateEntidadInterm(data,entidadInterm,entidad,cod):
-    dtoActualizadoList=[] #lista de actualizados y creados
-    dtoIntermList= [] #lista de todas la entidadIntermedia correspondiente
-    modelos = { 
-    "actividadParametro":hlmodel.ActividadParametro,
-    "recomendacionParametro": hlmodel.RecomendacionParametro,
-    "tipoAnalisisParam":hlmodel.TipoAnalisisParam,
-    "tipoPlanParam": hlmodel.TipoPlanParam
+modelos = { 
+    "actividadParametro":ActividadParametro,
+    "recomendacionParametro":RecomendacionParametro,
+    "tipoAnalisisParam":TipoAnalisisParam,
+    "tipoPlanParam": TipoPlanParam
     }
 
-    if entidad=='actividadParametro':
-        filtro = entidadInterm.codActividad
+def cantAsociaciones(entidad,entidadInterm):
+    if entidadInterm=='actividadParametro':
+        cantidad = ActividadParametro.query.filter(ActividadParametro.actividad==entidad).filter(ActividadParametro.isActiv==True).count()
     else:
         if entidad == 'recomendacionParametro': 
-            filtro = entidadInterm.codRecomendacion
+            cantidad = RecomendacionParametro.query.filter(RecomendacionParametro.recomendacion==entidad).filter(ActividadParametro.isActiv==True).count()
         else:
             if entidad == 'tipoAnalisisParam':
-                filtro = entidadInterm.codTipoAnalisis  
+                cantidad = TipoAnalisisParam.query.filter(TipoAnalisisParam.tipoAnalisis==entidad).filter(ActividadParametro.isActiv==True).count() 
             else:
                 if entidad == 'tipoAnalisisParam':
-                    filtro = entidadInterm.codTipoPlan  
-                else:
-                     raise Exception('N','No existe el codigo especial ingresado')
-
-    parametros = data.get('parametros')  
-
-    for param in parametros:
-        #isActiv = param.get('isActiv')
-        codParam = param.get('codParametro')
-        objeto = entidadInterm.query.filter(entidadInterm.codParametro==codParam).filter(filtro==cod).first() 
-        if objeto: #si existe, actualizo
-            dtoAux=dict(codParam = codParam, codNomen = cod)
-            objeto.isActiv = True
-            dtoActualizadoList.append(dtoAux)
-        else: # si no creo
-            dtoAux=dict(codParam = codParam, codNomen = cod)
-            objeto = modelos[entidad](True,codParam,cod)
-            dtoActualizadoList.append(dtoAux)
-            saveEntidadSinCommit(objeto)
-
-
-    intermediaList = entidadInterm.query.filter(filtro==cod).all() 
-
-    #creo diccionarios de los elementos que existen
-    for interm in intermediaList:
-        dtoInterm=dict(codParam = interm.codParametro)
-        if entidad=='actividadParametro':
-            dtoInterm['codNomen'] = interm.codActividad
-        else:
-            if entidad == 'recomendacionParametro': 
-                dtoInterm['codNomen'] = interm.codRecomendacion
-            else:
-                if entidad == 'tipoAnalisisParam':
-                    dtoInterm['codNomen'] = interm.codTipoAnalisis 
-                else:
-                    if entidad == 'tipoAnalisisParam':
-                        dtoInterm['codNomen'] = interm.codTipoPlan
-        dtoIntermList.append(dtoInterm)
- 
-
-    for item in dtoIntermList:
-        if not item in dtoActualizadoList: #si no fue actualizado ni creado, se coloca el false
-            objeto = entidadInterm.query.filter(entidadInterm.codParametro==item['codParam']).filter(filtro==item['codNomen']).first() 
-            objeto.isActiv =False 
-
-
-    Commit()
-    return ()
-
-
-
-def selectByisActiv(entidad,valor):
-    objetos = entidad.query.filter(entidad.isActiv==valor).all()
-    return objetos
+                    cantidad = TipoPlanParam.query.filter(TipoPlanParam.tipoPlan==entidad).filter(ActividadParametro.isActiv==True).count()  
+    return cantidad
