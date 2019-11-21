63fa1360cc65ef041e97a7356c40c2f5b4d4a275
Jafi <jafibravin@outlook.com>
2019-11-20 22:19:41 -0300

    Added comment to test gitlab webhook

--- ui/src/app/pantalla/pantalla.component.ts
+++ ui/src/app/pantalla/pantalla.component.ts
@@ -10,6 +10,7 @@ export class PantallaComponent implements OnInit {
   tokenExist = false;
 
   constructor(private router: Router) { 
+    // #TODO: Improve session detection
     if( localStorage.getItem('token') ){
       this.tokenExist = true;
       this.router.navigateByUrl('/home');
