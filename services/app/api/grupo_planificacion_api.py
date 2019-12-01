d9350084fc0f808f4b51103d3b80b8ba0db1ed0e
Chechi <chechi.fx2@gmail.com>
2019-12-01 02:32:02 -0300

    Filtro get grupos

--- services/app/api/grupo_planificacion_api.py
+++ services/app/api/grupo_planificacion_api.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from flask_restplus import Resource
 from app.api.helperApi.hlUrl import urlGrupoPlanificacion
-from app.uses_cases.moduloPlanificacion.gestionarGrupo import getPlanificaciones, getCuadros
+from app.uses_cases.moduloPlanificacion.gestionarGrupo import getPlanificaciones, getGrupos
 from app.api.shared.tokenHandler import token_required
 
 
@@ -12,7 +12,7 @@ grupoPlanificacion = urlGrupoPlanificacion
 class GrupoPlanificacionHandler(Resource):
     @token_required
     def get(data,currentUser,cod):
-        return getCuadros(currentUser,cod)
+        return getGrupos(currentUser,cod)
     
 @grupoPlanificacion.route('')
 class GrupoPlanificacionHandler(Resource):
